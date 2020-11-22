e67e1f8d363f9dcf7c714ef9acfa2d2e8bccac43
cmarker <cmarker4@gmail.com>
2020-11-21 18:50:27 -0600

    commit tree e829a4c3bfce3f2c5e287c8b26580a130e8f0019

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,12 +20,14 @@ class Triangle
   end
 
   def valid_triangle?
-    if @s1 + @s2 + @s3 > 2
-      return
-    elsif positive?(@s3) || positive?(@s1) || positive?(@s2)
-      return
-    else
+    if @s1 + @s2 <= @s3 || @s1 + @s3 <= @s2 || @s2 + @s3 <= @s1
       raise TriangleError
+    elsif @s1 == 0 || @s2 == 0 || @s3 == 0
+      raise TriangleError
+    elsif @s1 < 0 || @s2 < 0 || @s3 < 0
+      raise TriangleError
+    else
+      return
     end
   end
 
